e88c8fd2cc4413ac3815b28fb7c99c02e6b9a4b8
Agmar Torres <agmartorres@gmail.com>
2022-11-20 05:38:08 -0500

    feat: added random functions

---
+++ util/random.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func RandomString(number int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < number; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func randomEmail() string {
+	return fmt.Sprintf("%s@gmail.com", RandomString(6))
+
+}
